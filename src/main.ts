1f673dc4eb30688d743398421216cb0d579f2b35
linhnv-1913 <nguyen.van.linh-c@sun-asterisk.com>
2025-10-14 15:33:41 +0700

    Add API document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
+import { DEFAULT_VERSION } from './constants';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: DEFAULT_VERSION,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Medium Clone API')
+    .setDescription('API documentation for the Medium application')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  await app.listen(port);
 }
+
 bootstrap();
