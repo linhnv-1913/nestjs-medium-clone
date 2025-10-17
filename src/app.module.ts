65a54f0e34b8fc804461f6364a22369acf73d3b8
linhnv-1913 <nguyen.van.linh-c@sun-asterisk.com>
2025-10-17 10:59:52 +0700

    feat#init_model_and_relationship

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticlesService } from './articles/articles.service';
+import { ArticlesController } from './articles/articles.controller';
+import { ArticlesModule } from './articles/articles.module';
+import { dataSourceOptions } from './database';
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    ArticlesModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+  ],
+  controllers: [AppController, ArticlesController, UsersController],
+  providers: [AppService, ArticlesService, UsersService],
 })
 export class AppModule {}
